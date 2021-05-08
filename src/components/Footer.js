3853276117db0ce343b1c9356950c72d1bc4047d
Laura Gao <lauragao86868@gmail.com>
2021-05-08 18:51:06 -0400

    add philosophy to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,10 @@ import React from 'react'
 function Footer() {
     return (
         
-      <footer><a href="https://twitter.com/laurgao" target="_blank" rel="noreferrer">Twitter</a></footer>
+      <footer>
+        <a href="https://github.com/laurgao/prioritizer/blob/main/README.md" target="_blank" rel="noreferrer">Philosophy</a>
+        <a href="https://twitter.com/laurgao" target="_blank" rel="noreferrer">Twitter</a>
+      </footer>
     )
 }
 
